73c84b50c2ea629265160f55cd22c184e12dad6a
root <bbrutalhhonesty@gmail.com>
2016-03-23 00:14:11 -0400

    Added main file with example file header

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * 	@file 		main.cpp
+ * 	@author   Terry Thibault (tlt@cise.ufl.edu)
+ * 	@date 		3/22/16
+ * 	@version 	1.0
+ *
+ *  @brief 		COP3503 Project, Texas Hold'em
+ *
+ *  @section DESCRIPTION
+ *
+ *  This project is authored by:
+ *  	Diego Amador
+ *  	Tim Mai
+ *  	Cameron Mockabee
+ *  	Alexander Rumak
+ *  	Terry Thibault
+ *  	Hugh Wu
+ *
+ */
