56563baf11291431c6175fc9db958c5c14d73dcc
root <bbrutalhhonesty@gmail.com>
2016-03-26 16:49:46 -0400

    Header content added

--- header.h
+++ header.h
@@ -25,7 +25,7 @@
 #include <string>
 
 /*********************************************************
- * @brief The Table class maintains all the required 
+ * @brief The Table object maintains all the required 
  * information that is public domain (That can be accessed
  * by everyone playing). The Table class also maintains
  * the deck of cards.
@@ -40,6 +40,38 @@ class Table
 		std::string[] people;
 	
 	public:
-}
+		createNewPot(); // Creates a new pot to deal with extraneous conditions
+		addMoney(int amount); // Adds bet amount to pot
+		drawCard(); // Draws card from the Deck object
+		resetDeck(); // Declares a new Deck object
+		distributePot(); //Distributes the money in pot to the appropriate players	
+};
 
+/*********************************************************
+ * @brief The Deck object manages the cards in a deck.
+ *
+ ********************************************************/
+
+class Deck
+		std::vector<Card> Deck(52);
+		// Note: the syntax for assigning elements of the vector is the
+		// same as assigning elements to an array. 	Deck[0] = etc..
+
+	public:
+		shuffleDeck();
+		createDeck();
+};
+
+/*********************************************************
+ * @brief The Card object manages the values of the cards.
+ *
+ ********************************************************/
+
+	private:
+class Card
+{
+	private:
+		char suit;
+		int value; // Allow jack to be 11, Queen 12, King 13, Ace 14 or 1.	
+};
 #endif
