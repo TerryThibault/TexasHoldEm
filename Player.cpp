e16992bd3715e2264a456239240bf4599f05f371
defnotprobro <bbrutalhhonesty@gmail.com>
2016-04-11 18:00:32 -0400

    Added HasEnoughFunds

--- Player.cpp
+++ Player.cpp
@@ -1,2 +1,28 @@
-#include <string>
+/**
+ * 	@file       header.h
+ * 	@author(s)  Terry Thibault (tlt@cise.ufl.edu)
+ * 		    Alexander Rumak (alexrumak.ufl.edu)
+ 		    Hugh Wu
+ * 	@date       4/11/16
+ * 	@version    1.0
+ * 	@brief      COP3503 Project, Texas Hold'em
+ *
+ *
+ * 	@section DESCRIPTION
+ *
+ * 	This project is authored by:
+ * 		Diego Amador
+ * 		Tim Mai
+ * 		Cameron Mockabee
+ * 		Alexander Rumak
+ * 		Terry Thibault
+ * 		Hugh Wu
+ *
+ */
 
+ 
+bool Person::HasEnoughFunds(int bet)
+{
+	if (money < bet) return false;
+	return true;
+}
