83d6ab4756e8bd459fe2b6ab5457a05e366ac05e
defnotprobro <bbrutalhhonesty@gmail.com>
2016-04-11 18:42:27 -0400

    Added player functions

--- Player.cpp
+++ Player.cpp
@@ -20,9 +20,55 @@
  *
  */
 
- 
+/*********************************************************
+ * @brief This function checks if the player has enough 
+ * funds to make a bet. 
+ ********************************************************/
 bool Person::HasEnoughFunds(int bet)
 {
 	if (money < bet) return false;
 	return true;
+}
+
+/*********************************************************
+ * @brief This function will add money to the player money
+ * in the event that they win a hand.
+ ********************************************************/
+void addMoney(int amount) 
+{
+	money += amount;
+}
+
+/*********************************************************
+ * @brief This function is called by the table. Simply 
+ * subtracts from the player cash and tells the table
+ * how much to add to the pot. If the player does not have
+ * enough money, they go all-in.
+ ********************************************************/
+int bet(int amount)
+{
+	if(HasEnoughFunds(amount))
+	{
+		money -= amount;
+		return amount;
+	}
+	int temp = money;
+	money = 0;
+	return temp;
+}
+
+/*********************************************************
+ * @brief This function's input is the largest previous 
+ * bet. If the player does not have enough money to call,
+ * they will go all-in. 
+ ********************************************************/
+void call(int prev_bet)
+{
+	if(HasEnoughFunds(prev_bet))
+	{
+		money -= amount;
+		return;
+	}
+	money = 0;
+	return;
 }
