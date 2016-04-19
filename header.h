ee004d21b8c67314521ce0edb4afd9126444d066
Alexander Rumak <alexanderrumak@gmail.com>
2016-04-18 21:17:53 -0400

    Added a distributePot table function which will distribute the potand reset the pot to be empty.

--- header.h
+++ header.h
@@ -64,6 +64,7 @@ class Table
 		int getSmallBlindAmount(); //Returns the small Blind Amount
 
 		void newRound(); //Resets all table values for a new round of Poker
+		void distributePot(std::vector<Card> communityHand, int *pot);
     
         void print_computer(); //prints computer action
         void print_player(); //prints player action
@@ -72,7 +73,6 @@ class Table
 		//Table constructor
 		Table(std::vector<Player*> players, int smallBlindAmount, int gameSpeed);
 		~Table();
-		int turn(int betToMatch); // Turn will be the main player input mechanism. 
 };
 
 
