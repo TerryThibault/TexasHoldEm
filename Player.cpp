6d282a0efdac6fd14b7ba8512c0882d7c34ee7af
root <bbrutalhhonesty@gmail.com>
2016-04-19 20:28:25 -0400

    debugging

--- Player.cpp
+++ Player.cpp
@@ -215,7 +215,7 @@ bool isInt(std::string input)
 {
 	for(int i = 0; i < input.length(); ++i)
 	{
-		if(!(input[i] == "0" || input[i] == "1" || input[i] == "2" || input[i] == "3" || input[i] == "4"|| input[i] == "5" || input[i] == "6" || input[i] == "7" || input[i] == "8" || input[i] =="9"))
+		if(!(input[i] == '0' || input[i] == '1' || input[i] == '2' || input[i] == '3' || input[i] == '4' || input[i] == '5' || input[i] == '6' || input[i] == '7' || input[i] == '8' || input[i] == '9'))
 		{
 			return false;
 		}
